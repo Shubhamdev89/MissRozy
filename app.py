d506a1e4fa05739981170ca33baf2fde79076644
Shubhamdev89 <119775828+Shubhamdev89@users.noreply.github.com>
2023-03-25 13:06:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-#Thank you LazyDeveloper for helping me in this journey !
-#Must Subscribe On YouTube @LazyDeveloperr 
+#Thank you Shubhamdev64 for helping me in this journey !
+#Must Subscribe On YouTube @hackerbhai1
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '@LazyDeveloper'
+    return '@Shubhamdev64'
 
 if __name__ == "__main__":
     app.run()
